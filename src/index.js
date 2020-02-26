9c4f846a774d2c9bdf3508eeb6aa42dce63640ac
pashaSok <syffiks1998@gmail.com>
2020-02-26 10:41:43 +0300

    feat: create function min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length ==0){
+    return 0;
+   }
+   else{
+    let min = array[0];
+    if(array.length==0 || arguments.length ==0){
+      min=0;
+    }
+    for(let i=0;i<array.length;i++){
+      if(min>array[i]){
+        min=array[i];
+      }
+    }
+    return min;
+   }
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = 0;
+  if(arguments.length ==0){
+    return 0;
+   }
+  else {
+    for (let i = 0; i < array.length; i++) {
+    if(max<array[i]){
+      max=array[i];
+    }
+  }
+  return max;}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum=0;
+  let sred=0;
+  if(arguments.length ==0){
+   return 0;
+  }
+  else{
+    for(let i=0;i<array.length;i++){
+      sum+=array[i];
+      sred = sum/array.length;
+    }
+  return sred;
+  }
 }
